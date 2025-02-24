2184f6eaa45587bea9bd4c7dc9a957b1187fabb4
sudohelal <helaluddin40@outlook.com>
2025-02-24 22:32:09 +0600

    complete Testimonials section

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import Hero from './components/Hero/Hero'
 import Programs from './components/Programs/Programs'
 import Title from './components/Title/Title'
 import About from './components/About/About'
+import Campus from './components/Campus/Campus'
+import Testimonials from './components/Testimonials/Testimonials'
 
 const App = () => {
   return (
@@ -14,6 +16,11 @@ const App = () => {
         <Title subTitle='Our PROGRAM' title='What We Offer'></Title>
       <Programs></Programs>
       <About></About>
+      <Title subTitle='Gallery' title='Campus Photos'></Title>
+      <Campus></Campus>
+      <Title subTitle='TESTIMONIALS' title='What Student Says'></Title>
+      <Testimonials></Testimonials>
+      <Title subTitle='Contact Us' title='Get in Touch'></Title>
       </div>
       
     </div>
